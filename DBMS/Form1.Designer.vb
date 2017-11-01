91001d3a5f864068907ad03a4c23fb4457c631bc
Rakshith J <rakshithj1997@gmail.com>
2017-11-02 01:47:13 +0530

    Added User side app codes

---
+++ DBMS/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(login))
+        Me.login_button = New System.Windows.Forms.Button()
+        Me.userid = New System.Windows.Forms.TextBox()
+        Me.passwd = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'login_button
+        '
+        Me.login_button.BackColor = System.Drawing.Color.Lime
+        Me.login_button.Location = New System.Drawing.Point(467, 329)
+        Me.login_button.Name = "login_button"
+        Me.login_button.Size = New System.Drawing.Size(75, 23)
+        Me.login_button.TabIndex = 1
+        Me.login_button.Text = "Login"
+        Me.login_button.UseVisualStyleBackColor = False
+        '
+        'userid
+        '
+        Me.userid.Location = New System.Drawing.Point(466, 249)
+        Me.userid.Name = "userid"
+        Me.userid.Size = New System.Drawing.Size(100, 20)
+        Me.userid.TabIndex = 2
+        '
+        'passwd
+        '
+        Me.passwd.ForeColor = System.Drawing.Color.Black
+        Me.passwd.Location = New System.Drawing.Point(465, 291)
+        Me.passwd.Name = "passwd"
+        Me.passwd.Size = New System.Drawing.Size(100, 20)
+        Me.passwd.TabIndex = 3
+        Me.passwd.UseSystemPasswordChar = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(360, 246)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 23)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Enter your Id :"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(360, 288)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 23)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Password :"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.LinkLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.LinkLabel1.Location = New System.Drawing.Point(506, 386)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(70, 13)
+        Me.LinkLabel1.TabIndex = 7
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Register here"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Label3.Location = New System.Drawing.Point(444, 386)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "New User ? "
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(1, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(1018, 43)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Travel Info System"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Label5.Location = New System.Drawing.Point(415, 399)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(95, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Forgot Password ?"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.LinkLabel2.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.LinkLabel2.Location = New System.Drawing.Point(506, 399)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(59, 13)
+        Me.LinkLabel2.TabIndex = 11
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Reset here"
+        '
+        'login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1017, 496)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.passwd)
+        Me.Controls.Add(Me.userid)
+        Me.Controls.Add(Me.login_button)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents login_button As Button
+    Friend WithEvents userid As TextBox
+    Friend WithEvents passwd As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ DBMS/Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class login
+    Dim cn As New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Public user_id As String
+    Private Sub login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cn.ConnectionString = "server = localhost; userid=exp; password=exp.12345; database=dbms;"
+        cn.Open()
+        passwd.Enabled = True
+        userid.Enabled = True
+    End Sub
+
+    Private Sub login_button_Click_1(sender As Object, e As EventArgs) Handles login_button.Click
+        Dim input As String
+        Dim pass As String
+        Dim user As String
+        Dim rd As MySqlDataReader
+        input = passwd.Text
+        pass = Nothing
+        input = passwd.Text
+            user = userid.Text
+        cmd.Connection = cn
+        If check_Click() = 1 Then
+            cmd.CommandText = "SELECT passwords from userdb where userid = @userid ;"
+            cmd.Parameters.AddWithValue("@userid", user)
+            cmd.Prepare()
+            rd = cmd.ExecuteReader
+            If rd.Read Then
+                pass = rd.GetString(0)
+            End If
+            rd.Close()
+            cmd.Parameters.Clear()
+            If String.Compare(pass, input) = 0 Then
+                user_id = userid.Text
+                MsgBox("Welcome back " & user & "!")
+                My.Forms.homepage.Show()
+                My.Forms.login.Close()
+            Else
+                MsgBox("Incorrect password.Enter again")
+                passwd.Clear()
+            End If
+        End If
+    End Sub
+
+    Function check_Click() As Int16
+        Dim user As String
+        Dim input As String
+        Dim rd As MySqlDataReader
+        input = userid.Text
+        cmd.Connection = cn
+        cmd.CommandText = "SELECT userid  FROM userdb where userid = @input ;"
+        cmd.Parameters.AddWithValue("@input", input)
+        rd = cmd.ExecuteReader
+        If rd.Read Then
+            user = rd.GetString(0)
+        End If
+        cmd.Parameters.Clear()
+        rd.Close()
+        If String.IsNullOrEmpty(user) Then
+            MsgBox("Enter valid userID")
+            userid.Clear()
+            passwd.Clear()
+            Return 0
+        End If
+        Return 1
+    End Function
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        My.Forms.register_page.Show()
+        My.Forms.login.Close()
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        My.Forms.forgot_pass.Show()
+        My.Forms.login.Close()
+    End Sub
+
+End Class
